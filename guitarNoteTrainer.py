2fa6059bb026552634e16d5dd2e66d95a981d199
brandon-viterbo <44447770+brandon-viterbo@users.noreply.github.com>
2021-03-22 03:46:54 -0400

    Add files via upload
    Fixed silly mistake where I missed a positional argument in function menuLoop(). 
    All tab white spaces are now four spaces. 
    File "guitar.json" is now initialized to standard tuning isntead of open D 6/9 tuning.

--- guitarNoteTrainer.py
+++ guitarNoteTrainer.py
@@ -12,49 +12,49 @@ EXIT_INPUT = "E"
 
 """
 TODO:
-1)	Make input loops for start menu
-		-Tune Guitar?
-		-Note identity test?
-		-Exit
+1)    Make input loops for start menu
+        -Tune Guitar?
+        -Note identity test?
+        -Exit
 """
 
 with open(NOTE_FILE, "r") as readFile:
-	notes = json.load(readFile)["notes"]
+    notes = json.load(readFile)["notes"]
 
 def menuLoop(notes):
-	with open(GUITAR_FILE, "r") as readFile:
-		guitar = json.load(readFile)
-		guitarStrings = guitar["strings"]
-		guitarTuning = guitar["tuningText"]
+    with open(GUITAR_FILE, "r") as readFile:
+        guitar = json.load(readFile)
+        guitarStrings = guitar["strings"]
+        guitarTuning = guitar["tuningText"]
 
-	print("Welcome to guitar note trainer! Your guitar is tuned to {}.".format(
-		guitarTuning)
-	)
-	userInput = input(("Press '{}' to test note identification, '{}' to " +  
-		"change your guitar's tuning, or '{}' to exit: ").format(
-			ID_INPUT, TUNE_INPUT, EXIT_INPUT)
-		)
+    print("Welcome to guitar note trainer! Your guitar is tuned to {}.".format(
+        guitarTuning)
+    )
+    userInput = input(("Press '{}' to test note identification, '{}' to " +  
+        "change your guitar's tuning, or '{}' to exit: ").format(
+            ID_INPUT, TUNE_INPUT, EXIT_INPUT)
+        )
 
-	userInput = userInput.upper()
-	if userInput == ID_INPUT:
-		return identifyNotesLoop(notes)
-	elif userInput == TUNE_INPUT:
-		print("\nNow tuning guitar.")
-		guitarTuner.tuneGuitar(notes)
-		print("\n")
-		return menuLoop()
-	elif userInput == EXIT_INPUT:
-		print("Thank you for using Guitar Note Trainer!")
-		print("See you later!")
-	else:
-		print("Invalid input. Please try again.")
-		return menuLoop()
-	return
+    userInput = userInput.upper()
+    if userInput == ID_INPUT:
+        return identifyNotesLoop(notes)
+    elif userInput == TUNE_INPUT:
+        print("\nNow tuning guitar.")
+        guitarTuner.tuneGuitar(notes)
+        print("\n")
+        return menuLoop(notes)
+    elif userInput == EXIT_INPUT:
+        print("Thank you for using Guitar Note Trainer!")
+        print("See you later!")
+    else:
+        print("Invalid input. Please try again.\n")
+        return menuLoop(notes)
+    return
 
 
 def identifyNotesLoop(notes):
-	print("TODO: Test Note Identification.")
-	return
+    print("TODO: Test Note Identification.")
+    return
 
 menuLoop(notes)
 
