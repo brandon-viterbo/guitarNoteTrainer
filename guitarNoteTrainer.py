c4d6ff5cdd65c28237381d8e716246f8cbc1a0a8
brandon-viterbo <44447770+brandon-viterbo@users.noreply.github.com>
2021-03-22 03:32:37 -0400

    Add files via upload
    Notes scaled across fretboard. 
    TODO:
    Work on input loops to test user on note identification.

---
+++ guitarNoteTrainer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import guitarTuner
+import random
+import json
+
+GUITAR_FILE = "guitar.json"
+NOTE_FILE = "equalTempermentNotes.json"
+TUNE_INPUT = "T"
+ID_INPUT = "I"
+EXIT_INPUT = "E"
+
+"""
+TODO:
+1)	Make input loops for start menu
+		-Tune Guitar?
+		-Note identity test?
+		-Exit
+"""
+
+with open(NOTE_FILE, "r") as readFile:
+	notes = json.load(readFile)["notes"]
+
+def menuLoop(notes):
+	with open(GUITAR_FILE, "r") as readFile:
+		guitar = json.load(readFile)
+		guitarStrings = guitar["strings"]
+		guitarTuning = guitar["tuningText"]
+
+	print("Welcome to guitar note trainer! Your guitar is tuned to {}.".format(
+		guitarTuning)
+	)
+	userInput = input(("Press '{}' to test note identification, '{}' to " +  
+		"change your guitar's tuning, or '{}' to exit: ").format(
+			ID_INPUT, TUNE_INPUT, EXIT_INPUT)
+		)
+
+	userInput = userInput.upper()
+	if userInput == ID_INPUT:
+		return identifyNotesLoop(notes)
+	elif userInput == TUNE_INPUT:
+		print("\nNow tuning guitar.")
+		guitarTuner.tuneGuitar(notes)
+		print("\n")
+		return menuLoop()
+	elif userInput == EXIT_INPUT:
+		print("Thank you for using Guitar Note Trainer!")
+		print("See you later!")
+	else:
+		print("Invalid input. Please try again.")
+		return menuLoop()
+	return
+
+
+def identifyNotesLoop(notes):
+	print("TODO: Test Note Identification.")
+	return
+
+menuLoop(notes)
+
+exit()
--- guitarTuner.py
+++ guitarTuner.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
 """
-TODO:
-    -Condense the prompt loop functions into one function.
-    -Change tune guitar string function so that it saves the other names of
-    the notes.
+Coded with Python 3.7.4
 """
 
 import json
+import itertools
 
 GUITAR_FILE = "guitar.json"
-NOTE_FILE = "equal_temperment_notes.json"
+NOTE_FILE = "equalTempermentNotes.json"
 
-with open(NOTE_FILE, "r") as read_file:
+with open(NOTE_FILE, "r") as readFile:
     """
     The variable "notes" is a list of lists, where each sublist represents a note
     and each element in that sublist is a valid string name for that note. 
@@ -20,7 +18,7 @@ with open(NOTE_FILE, "r") as read_file:
     "C" is represented with the sublist ["C", "B#"].
     All function definitions that have "notes" will call this variable.  
     """
-    notes = json.load(read_file)["notes"]
+    notes = json.load(readFile)["notes"]
 
 
 def tuneGuitar(notes):
@@ -28,16 +26,37 @@ def tuneGuitar(notes):
     Main function of this document. Writes whatever tuning the user inputs when 
     tuneGuitarPrompt() is called into the file defined as GUITAR_FILE. 
     """
-    tuning = tuneGuitarPrompt(notes)
+    tuningIndexes, tuningText, frets = tuneGuitarPrompt(notes)
 
-    with open(GUITAR_FILE, "w") as write_file:
+    with open(GUITAR_FILE, "w") as writeFile:
         count = 0
-        guitarStrings = [None]*len(tuning)
-        for newTuning in tuning[::-1]:
-            guitarStrings[count] = {"stringNo": count+1, "openNote": newTuning}
+        guitarStrings = [None]*len(tuningIndexes)
+        for openNoteIndex in tuningIndexes[::-1]:
+            notesOnString = determineNotesOnString(openNoteIndex, frets)
+            guitarStrings[count] = {"stringNo": count+1, 
+                "notesOnString": notesOnString
+            }
             count += 1
         data = {"strings": guitarStrings}
-        json.dump(data, write_file, indent=4)
+        data["tuningText"] = tuningText
+        data["frets"] = frets
+        json.dump(data, writeFile, indent=4)
+
+    return data
+
+
+def determineNotesOnString(openNoteIndex, frets):
+    notesOnString = [openNoteIndex]*(frets+1) # +1 accounts for the open note
+    notesCycle = itertools.cycle(notes)
+
+    currentCycle = openNoteIndex
+    for i in range(openNoteIndex+1):
+        currentCycle = next(notesCycle)
+    
+    for i in range(0, frets+1):
+        notesOnString[i] = currentCycle
+        currentCycle = next(notesCycle)
+    return notesOnString
 
 
 def tuneGuitarPrompt(notes):
@@ -48,30 +67,50 @@ def tuneGuitarPrompt(notes):
     stringNumber = getGuitarStringInput()
     print("Your guitar has {} strings.".format(stringNumber))
 
-    tuning=[None]*stringNumber
+    tuningIndexes=[None]*stringNumber
+    tuningText=[None]*stringNumber
     for guitarString in range(stringNumber):
-        inputNote = tuneGuitarStringInput(guitarString, notes)
-        tuning[stringNumber-guitarString-1] = inputNote
+        inputNote, noteNumber = tuneGuitarStringInput(guitarString, notes)
+        tuningIndexes[stringNumber-guitarString-1] = noteNumber
+        tuningText[stringNumber-guitarString-1] = inputNote
 
-    tuningStr = "".join(tuning)
+    tuningText = "".join(tuningText)
     print("From strings {} to 1, your guitar is tuned as {}.".format(
-        stringNumber, tuningStr)
+        stringNumber, tuningText)
     )
-    return tuning
+
+    frets = fretboardPrompt()
+    return tuningIndexes, tuningText, frets
 
 
 """
 Following two functions are input prompts that loop until the user inputs a
 valid response.
 """
+def fretboardPrompt():
+    frets = input("Finally, how many frets does your guitar have? ")
+    try:
+        frets = int(frets)
+        if frets < 0:
+            #Go to except branch if frets is an int less than zero.
+            int("a")
+    except:
+        print("Please enter an integer greater than or equal to zero.")
+        return fretboardPrompt()
+    
+    print("Your guitar has {} fret(s).".format(frets))
+    return frets
+
+
 def tuneGuitarStringInput(guitarString, notes):
     inputNote = input("What is string {} tuned to? ".format(guitarString+1))
-    if validNote(inputNote, notes):
-        return inputNote
+    noteNumber = validNote(inputNote, notes)
+    if noteNumber > -1:
+        return inputNote, noteNumber
     else:
         print("Please enter a note as the capitalized note letter on its own ",
             "or followed by '#' or lowercase 'b'.")
-        return tuneGuitarString(guitarString, notes)
+        return tuneGuitarStringInput(guitarString, notes)
 
 
 def getGuitarStringInput():
@@ -92,10 +131,9 @@ def validNote(inputNote, notes):
     Checks if string "inputNote" is a note in the note system (i.e. twelve-tone equal
     temperment) specified in the list of lists "notes". 
     """
+    index = 0
     for note in notes:
         if inputNote in note:
-            return True
-    return False
-
-
-tuneGuitar(notes)
+            return index
+        index += 1
+    return -1
