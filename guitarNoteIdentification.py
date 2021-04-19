106c31f9cc81766e805badc977e80e241f73d49d
brandon-viterbo <44447770+brandon-viterbo@users.noreply.github.com>
2021-04-19 03:18:32 -0400

    Add files via upload
    Replace TAB with 4 spaces

--- guitarNoteIdentification.py
+++ guitarNoteIdentification.py
@@ -6,46 +6,46 @@ GUITAR_FILE = "guitar.json"
 MAIN_FILE = "guitarNoteTrainer.py"
 
 def test(notes, guitarStrings, frets):
-	testLength = input("\nHow many questions long should this test to be? ")
+    testLength = input("\nHow many questions long should this test to be? ")
 
-	try:
-		testLength = int(testLength)
-		if testLength < 1:
-			int("a")
-	except:
-		print("Please enter an integer greater than 0.")
-		return test(notes, guitarStrings, frets)
+    try:
+        testLength = int(testLength)
+        if testLength < 1:
+            int("a")
+    except:
+        print("Please enter an integer greater than 0.")
+        return test(notes, guitarStrings, frets)
 
-	print("Please enter notes as the capitalized note letter on its own ",
+    print("Please enter notes as the capitalized note letter on its own ",
         "or followed by '#' or lowercase 'b'.")
-	score = 0
-	for i in range(testLength):
-		answer = noteIdentification(notes, guitarStrings, frets)
-		if answer:
-			score += 1
+    score = 0
+    for i in range(testLength):
+        answer = noteIdentification(notes, guitarStrings, frets)
+        if answer:
+            score += 1
 
-	print("\nTest complete! Your score is {}/{}.".format(score, testLength))
+    print("\nTest complete! Your score is {}/{}.".format(score, testLength))
 
 
 def noteIdentification(notes, guitarStrings, frets):
-	"""
-	For string values in square brackets, make sure guitar is tuned by running
-	MAIN_FILE, then check GUITAR_FILE
-	"""
-	string = random.choice(guitarStrings)
-	stringNo = string["stringNo"]
-	fretNumber = random.randint(0, frets)
-	noteText = string["notesOnString"][fretNumber]
+    """
+    For string values in square brackets, make sure guitar is tuned by running
+    MAIN_FILE, then check GUITAR_FILE
+    """
+    string = random.choice(guitarStrings)
+    stringNo = string["stringNo"]
+    fretNumber = random.randint(0, frets)
+    noteText = string["notesOnString"][fretNumber]
 
-	answer = input("\nWhat note is on string {} and fret {}? ".format(
-		stringNo, fretNumber)
-	)
+    answer = input("\nWhat note is on string {} and fret {}? ".format(
+        stringNo, fretNumber)
+    )
 
-	if answer in noteText:
-		print("Correct.")
-		return True
-	else:
-		print("Wrong.")
-		return False
-	
+    if answer in noteText:
+        print("Correct.")
+        return True
+    else:
+        print("Wrong.")
+        return False
+    
 
